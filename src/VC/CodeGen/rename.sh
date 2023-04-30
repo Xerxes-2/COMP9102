539058923d68b3cf059b2843b4e71c402b8b2e5c
Shupeng Xue <dspxue@gmail.com>
2023-04-30 18:45:59 +1000

    import test set

--- src/VC/CodeGen/rename.sh
+++ src/VC/CodeGen/rename.sh
@@ -2,6 +2,16 @@
 
 shopt -s nullglob
 
+for file in test[0-9]*; do
+    if [[ -f "$file" ]]; then
+        newname=$file.vc
+        mv "$file" "$newname"
+        echo "Renamed file $file to $newname"
+    fi
+done
+
+shopt -s nullglob
+
 for file in result[0-9]*; do
     if [[ -f "$file" ]]; then
         newname=$(echo "$file" | sed 's/^result/test/' | sed 's/$/.sol/')
